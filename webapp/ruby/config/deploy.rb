ca8c1b112e8b2a03492023aa008adf152504d5c9
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-11-08 11:47:42 +0900

    add cap

---
+++ webapp/ruby/config/deploy.rb
@@ -0,0 +1,137 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+# set :application, 'my_app_name'
+# set :repo_url, 'git@example.com:me/my_repo.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# namespace :deploy do
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       # Your restart mechanism here, for example:
+#       # execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
+
+#   after :publishing, :restart
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+
+# end
+
+set :application, ''
+set :repo_url, 'git@github.com:tomoasleep/isucon4.git'
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :deploy_to, '/home/isucon/isucon3'
+set :scm, :git
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+set :deploy_via, :remote_cache
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "cd #{current_path}/ruby;. /etc/env.sh; bundle install"
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      # execute :rake, 'cache:clear'
+      # end
+    end
+  end
+  after :finishing, 'deploy:cleanup'
+end
+
+task :bench do
+  on roles(:bench) do
+    execute "/home/isucon/benchmarker bench --hosts 203.104.111.163 --workload 2"
+  end
+end
+
+
+namespace :nginx do
+  task :reload do
+    on roles(:web) do
+      execute :sudo, 'service nginx reload'
+    end
+  end
+  task :restart do
+    on roles(:web) do
+      execute :sudo, 'service nginx restart'
+    end
+  end
+  task :start do
+    on roles(:web) do
+      execute :sudo, 'service nginx start'
+    end
+  end
+end
+namespace :supervisor do
+  task :restart do
+    on roles(:app) do
+      execute :sudo, 'supervisorctl restart isucon_ruby'
+    end
+  end
+end
+namespace :mysql do
+  task :reload do
+    on roles(:db) do
+      execute :sudo, 'rm -f /tmp/slow-query.log'
+      execute :sudo, 'service mysqld reload' # isucon2 ... mysqld
+    end
+  end
+  task :restart do
+    on roles(:db) do
+      execute :sudo, 'rm -f /tmp/slow-query.log'
+      execute :sudo, 'service mysqld restart' # isucon2 ... mysqld
+    end
+  end
+end
---
+++ webapp/ruby/config/deploy/production.rb
@@ -0,0 +1,91 @@
+set :stage, :production
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db, %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a has can be used to set
+# extended properties on the server.
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+server '203.104.111.161', user: 'isucon', roles: 'app'
+server '203.104.111.162', user: 'isucon', roles: 'app'
+server '203.104.111.163', user: 'isucon', roles: ['app', 'bench']
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+set :ssh_options, {
+ keys: %w(~/.ssh/id_rsa.isucon),
+ forward_agent: true,
+ auth_methods: %w(publickey)
+}
+# and/or per server
+# server 'example.com',
+# user: 'user_name',
+# roles: %w{web app},
+# ssh_options: {
+# user: 'user_name', # overrides user setting above
+# keys: %w(/home/user_name/.ssh/id_rsa),
+# forward_agent: false,
+# auth_methods: %w(publickey password)
+# # password: 'please use keys'
+# }
+# setting per server overrides global ssh_options
+
+# fetch(:default_env).merge!(rails_env: :production)
+# # Simple Role Syntax
+# # ==================
+# # Supports bulk-adding hosts to roles, the primary server in each group
+# # is considered to be the first unless any hosts have the primary
+# # property set.  Don't declare `role :all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
+
+
+# # Extended Server Syntax
+# # ======================
+# # This can be used to drop a more detailed server definition into the
+# # server list. The second argument is a, or duck-types, Hash and is
+# # used to set extended properties on the server.
+
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+
+# # Custom SSH Options
+# # ==================
+# # You may pass any option but keep in mind that net/ssh understands a
+# # limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+# #
+# # Global options
+# # --------------
+# #  set :ssh_options, {
+# #    keys: %w(/home/rlisowski/.ssh/id_rsa),
+# #    forward_agent: false,
+# #    auth_methods: %w(password)
+# #  }
+# #
+# # And/or per server (overrides global)
+# # ------------------------------------
+# # server 'example.com',
+# #   user: 'user_name',
+# #   roles: %w{web app},
+# #   ssh_options: {
+# #     user: 'user_name', # overrides user setting above
+# #     keys: %w(/home/user_name/.ssh/id_rsa),
+# #     forward_agent: false,
+# #     auth_methods: %w(publickey password)
+# #     # password: 'please use keys'
+# #   }
