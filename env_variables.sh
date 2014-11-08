311d963fa5c185f2e13fc2cb6d0ea93361765d8e
Tomoya Chiba <tomo.asleep@gmail.com>
2014-11-08 11:24:38 +0900

    Initial Commit

---
+++ env_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
+export PATH=/home/isucon/.local/ruby/bin:$PATH
+export PATH=/home/isucon/.local/node/bin:$PATH
+export PATH=/home/isucon/.local/python/bin:$PATH
+export PATH=/home/isucon/.local/perl/bin:$PATH
+export PATH=/home/isucon/.local/php/bin:$PATH
+export PATH=/home/isucon/.local/php/sbin:$PATH
+export PATH=/home/isucon/.local/redis/bin:$PATH
+export GOPATH=/home/isucon/gocode
+export ISUCON_TEAM=team_21
+export ISUCON_API_KEY=106-4-nenz41-14hn-b35e135f4c2ee0ced45c49e78e21e4cd2ac579c3
